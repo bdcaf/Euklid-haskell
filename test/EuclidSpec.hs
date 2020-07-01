83bd89cff7f55c9e9ce42bd8078b2c401feb7c68
Clemens Ager <clemens.ager@gmail.com>
2020-07-01 11:13:23 +0200

    test after review on SE

--- test/EuclidSpec.hs
+++ test/EuclidSpec.hs
@@ -8,43 +8,37 @@
 module EuclidSpec  ( spec )
 where
 
-import Test.Hspec
-import Test.Hspec.Core.QuickCheck
-import Test.QuickCheck
 import Lib
+import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
 
 spec :: Spec
 spec = do
   describe "Trivial" $ do
     it "trivial example 99 1" $
-       let trivial = extendedEuclid 99 1
-       in  trivial `shouldBe` (EuclidRes 1 (0) 1)
+       extendedEuclid 99 1 `shouldBe` (EuclidRes 1 0 1)
     it "trivial example 99 99" $
-       let trivial = extendedEuclid 99 99
-       in  trivial `shouldBe` (EuclidRes 99 (0) 1)
+       extendedEuclid 99 99 `shouldBe` (EuclidRes 99 0 1)
   describe "Examples" $ do
     it "explanation example 99 78" $
-       let wikiExample = extendedEuclid 99 78
-       in  wikiExample `shouldBe` (EuclidRes 3 (-11) 14)
+       extendedEuclid 99 78 `shouldBe` (EuclidRes 3 (-11) 14)
     it "explanation example flipped 78 99" $
-       let wikiExample = extendedEuclid 78 99
-       in  wikiExample `shouldBe` (EuclidRes 3 14 (-11) )
+       extendedEuclid 78 99  `shouldBe` (EuclidRes 3 14 (-11) )
     it "explanation example 99 78" $
-       let wikiExample = extendedEuclid 240 46
-       in  wikiExample `shouldBe` (EuclidRes 2 (-9) 47)
+       extendedEuclid 240 46 `shouldBe` (EuclidRes 2 (-9) 47)
   describe "properties" $ do
-      it "both numbers divisible a%gcd == 0, b%gcd ==0" $ property $
+      prop "both numbers divisible a%gcd == 0, b%gcd == 0" $ 
             prop_divisible
-      it "bezout a*s+b*t = gcd" $ property $
+      prop "bezout a*s+b*t = gcd" $ 
             prop_bezout
-      it "recursive and iterative algorithm have same result" $ property $
-            prop_same_as_recursive
+      prop "recursive and iterative algorithm have same result" $ prop_same_as_recursive
 
-prop_divisible a b = a>0 && b>0 ==> a `mod` d ==0 && b `mod`d == 0
+prop_divisible (Positive a) (Positive b) =  a `mod` d ==0 && b `mod`d == 0
   where EuclidRes d s t = extendedEuclid a b
                              
-prop_bezout a b = a>0 && b>0 ==> a*s + b*t == d
+prop_bezout (Positive a) (Positive b) =  a*s + b*t == d
   where EuclidRes d s t = extendedEuclid a b
 
-prop_same_as_recursive a b = a>0 && b>0 ==> extendedEuclid a b == extendedEuclid' a b
+prop_same_as_recursive (Positive a) (Positive b) =  extendedEuclid a b == extendedEuclid' a b
                              
